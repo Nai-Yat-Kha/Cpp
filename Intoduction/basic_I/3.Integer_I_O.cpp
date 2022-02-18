a351c21090522187e7d55a39723a3f282c5e9ce9
Sit Ko Naing <sitkonaing22699@gmail.com>
2022-02-18 22:10:53 +0630

    Basic-Inpt-Output

---
+++ Intoduction/basic_I/3.Integer_I_O.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an interger : " ;
+    cin >> num ;
+    cout << "This number : " << num << endl;
+
+    return 0;
+}
---
+++ Intoduction/basic_I/4.Multiple_Inputs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a;
+    int num ;
+     cout << "Enter a character and an interger : ";
+     cin >> a >> num ;
+
+     cout << "Character : "<< a << endl;
+     cout << "Number : " << num << endl;
+
+     return 0;
+}
