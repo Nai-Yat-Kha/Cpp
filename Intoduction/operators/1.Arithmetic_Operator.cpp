e093a09ffdfcff8193761bb5e68bbaa28f2f83d9
Sit Ko Naing <sitkonaing22699@gmail.com>
2022-02-19 21:38:12 +0630

    Operators

---
+++ Intoduction/operators/1.Arithmetic_Operator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a , b;
+
+    a = 7;
+    b = 2;
+
+    cout << "plus : "<< (a+b)<<endl;
+
+    cout << "minus : "<< (a-b)<<endl;
+
+    cout << "multiply : "<<(a*b)<<endl;
+
+    cout << "divided : "<<(a/b)<<endl;
+
+    //cout << "zero : "<< (a%b)<<endl;
+
+    return 0;
+}
---
+++ Intoduction/operators/3.Assignment_operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a , b;
+    a = 2;
+    b = 3;
+    cout << "a : "<<a <<endl;
+    cout << "b : "<<b <<endl;
+
+    a +=b;
+
+    cout << a << endl;
+
+    return 0;
+    
+}
