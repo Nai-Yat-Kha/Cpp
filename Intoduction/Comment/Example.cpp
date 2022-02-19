5a56d6ed94aff81adafc5f6842a1677de7d0b331
Sit Ko Naing <sitkonaing22699@gmail.com>
2022-02-19 21:44:58 +0630

    Comment

---
+++ Intoduction/Comment/Example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "some code";
+   //cout << ''error code;
+   cout << "some other code";
+
+   return 0;
+}
+/*
+This is the comment .
+IN Cpp this section can't run when debug.
+// is comment for a code line.
+
+*/
